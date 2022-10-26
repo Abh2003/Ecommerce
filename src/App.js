b939baeaee16716aa1bf808299476d1bbecfde94
Abh2003 <abhishek1448.be20@chitkara.edu.in>
2022-10-26 22:39:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useThemeHook } from './GlobalComponents/ThemeProvider';
+import Header from './components/Header';
+import { Router } from "@reach/router";
+import Footer from "./components/Footer.js";
+
+//Pages
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+
+
+function App() {
+  const [theme] = useThemeHook();
+  return (
+    <main className={theme? 'bg-black': 'bg-light-2'} style={{ height: '100vh', overflowY: 'auto'}}>
+      <Header/>
+      <Router>
+        <Home path="/" />
+        <Cart path="/cart" />
+      </Router>
+      <Footer/>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+function Footer(){
+    return(
+        <>
+            <div className='fg-color fixed-footer'>
+                <p>
+                 <center>
+                  2022@ copyright ecart marketing
+                  </center>
+                  </p>
+            </div>
+        </>
+        );
+};
+
+
+export default Footer;
